b9fdc81b371c8ce59d42337654b4a31eb261b1ba
LewisMatos <lewm20@gmail.com>
2017-03-25 14:21:52 -0400

    add proc

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
 
-var db = new sqlite.Database(path.join(__dirname, '/database/database.sqlite'));
+var db = new sqlite.Database(path.join(__dirname, 'app/database/database.sqlite'));
 
 app.set('db', db);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, 'app/views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app/public')));
 app.use(bodyParser.json());
-app.use(require(path.join(__dirname, '/api/api')));
+app.use(require(path.join(__dirname, 'app/api/api')));
 
 app.get('/', function (req, res) {
   res.render('index');
